20238e6b25f8f4bf3b43f82cb58ee9b9c663055b
1eastar <ehdwls6703@gmail.com>
2020-07-23 21:18:29 +0900

    result

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import home, detail, new, create
+from .views import home, detail, create
 
 urlpatterns = [
     path('', home, name="home"),
     path('<int:blog_id>/', detail, name="detail"),
-    path('new/', new, name="new"),
     path('create/', create, name="create"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 
 from .models import Blog
+from .forms import BlogForm
 
 # Create your views here.
 
 def home(request):
-    blogs = Blog.objects.all()
+    blog_all = Blog.objects.all()
+    paginator = Paginator(blog_all, 3)
+    page = request.GET.get('page')
+    blogs = paginator.get_page(page)
     return render(request, 'home.html', {'blogs': blogs})
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
     return render(request, 'detail.html', {'blog': blog})
 
-def new(request):
-    return render(request, 'new.html')
-
 def create(request):
-    blog = Blog()
-    blog.title = request.GET['title']
-    blog.content = request.GET['content']
-    blog.save()
-    return redirect('/blog/'+str(blog.id))
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            post = form.save()
+            return redirect('/blog/'+str(post.id))
+    else:
+        form = BlogForm()
+        return render(request, 'new.html', {'form': form})
